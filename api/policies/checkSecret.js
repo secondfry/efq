ba17555253a03eba9c841832738553ebe4fd8f1e
Rustam Gubaydullin <secondfry@gmail.com>
2014-07-26 00:21:30 +0400

    checkSecret cookies fix

--- api/policies/checkSecret.js
+++ api/policies/checkSecret.js
@@ -1,5 +1,5 @@
 module.exports = function checkSecret (req, res, next) {
-  if (req.session.level == undefined && req.cookies.check) {
+  if (req.session.level == undefined && req.cookies && req.cookies.check) {
     Pilot.findOneByEveID(req.session.eveID).done(function(err, user){
       if (err) res.send(err); else if (user && user.secret == req.cookies.check) {
         req.session.level = user.level;
