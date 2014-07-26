be8e22c9b7f7367ccfd74b5b3b5e02ad2acc8655
Rustam Gubaydullin <secondfry@gmail.com>
2014-07-26 04:44:06 +0400

    Создание banList.json, levelList.json в случае их отсутствия

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,6 +10,23 @@
 
 module.exports.bootstrap = function (cb) {
 
+  /**
+   * Проверяет наличие файла и, если его нет, создает пустой JSON
+   * @param filepath Путь к файлу
+   * @param comment Комментарий внутри
+   */
+  function checkCreateJSON(filepath, comment) {
+    var fs = require('fs');
+    fs.exists(filepath, function(exists) {
+      if (!exists) fs.writeFile(filepath, '// ' + comment + '\n{}', function(err) {
+        if (err) sails.log.error(err); else sails.log.info('Created ' + filepath + '.');
+      });
+    });
+  }
+
+  checkCreateJSON('config/banList.json', '"%Имя персонажа%": "banned"');
+  checkCreateJSON('config/levelList.json', '"%Имя персонажа%": %Уровень доступа%');
+
   // It's very important to trigger this callack method when you are finished 
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
